ff49ff4b36cb79a73e57f3d3eae6358b9944d6d7
bard <bard@ppfriends.co.kr>
2024-06-08 21:42:26 +0900

    refactor: 월별로 분리

--- Autobill/View/ContentView.swift
+++ Autobill/View/ContentView.swift
@@ -11,19 +11,29 @@ import SwiftUI
 struct ContentView: View {
     
     @Environment(\.modelContext) private var context
-    @Query(sort: \BillImage.createdDate, order: .forward)
-    private var billImages: [BillImage] 
+    @Query(sort: \BillImage.date, order: .forward)
+    private var billImages: [BillImage]
     private let gridItem = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
-    
+
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: gridItem) {
-                    ForEach(billImages.indices, id: \.self) { index in
-                        NavigationLink(destination: ImageScrollView(presentIndex: index)) {
-                            Image(uiImage: billImages[index].image)
-                                .resizable()
-                                .scaledToFit()
+                LazyVGrid(columns: gridItem, alignment: .leading, pinnedViews: .sectionHeaders) {
+                    ForEach(groupedBillImages.keys.sorted(), id: \.self) { month in
+                        Section(
+                            header: Text(month)
+                                .font(.headline)
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 4)
+                                .background(Color.black.opacity(0.2))
+                        ) {
+                            ForEach(groupedBillImages[month]!.indices, id: \.self) { index in
+                                NavigationLink(destination: ImageScrollView(presentIndex: index)) {
+                                    Image(uiImage: groupedBillImages[month]![index].image)
+                                        .resizable()
+                                        .scaledToFit()
+                                }
+                            }
                         }
                     }
                 }
@@ -50,5 +60,17 @@ struct ContentView: View {
                 }
             }
         }
+    }
+    
+    private var groupedBillImages: [String: [BillImage]] {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM"
+        
+        let grouped = Dictionary(grouping: billImages) { (billImage) -> String in
+            let dateComponents = billImage.date.components(separatedBy: "-")
+            return "\(dateComponents[0])-\(dateComponents[1])"
+        }
+        
+        return grouped
     }
 }
