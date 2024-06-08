1f56967228fe6a2a0eed7156dddca5142ec511e4
bard <bard@ppfriends.co.kr>
2024-06-08 21:34:59 +0900

    refactor: 날짜 및 금액을 받기 위한 파라미터 추가

--- Autobill/Utils/BillImage.swift
+++ Autobill/Utils/BillImage.swift
@@ -14,13 +14,17 @@ final class BillImage {
     @Attribute(.unique) private(set) var id = UUID()
     
     var imageData: Data
+    var totalAmountText: String
+    var date: String
     private(set) var createdDate = Date()
     
     var image: UIImage {
         return UIImage(data: imageData) ?? UIImage(systemName: "photo.artframe")!
     }
     
-    init(image: UIImage) {
+    init(image: UIImage, totalAmountText: String, date: String) {
         self.imageData = image.jpegData(compressionQuality: 1.0) ?? Data()
+        self.totalAmountText = totalAmountText
+        self.date = date
     }
 }
